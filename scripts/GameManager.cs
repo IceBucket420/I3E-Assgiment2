68098fc01704fc9f880398cacfb9f76bddda6933
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-06-23 10:48:06 +0800

    Update GameManager.cs
    Let player spawn in to the game

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -1,16 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject PlayerPrefab;
+    private PlayerMovement activePlayer;
+    public static int Score;
 
-    // Update is called once per frame
+    //public static void StaticFnc()
+    public static GameManager instance;
+    //{
+    //    if (activePlayer = null)
+    //    {
+
+    //    }
+    //}
+
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        //DontDestroyOnLoad(gameObject);
+        //SceneManager.activeSceneChanged += SpawnPlayerOnLoad;
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            SceneManager.activeSceneChanged += SpawnPlayerOnLoad;
+
+            instance = this;
+        }
+    }
+    private void SpawnPlayerOnLoad(Scene prev, Scene next)
+    {
+        Debug.Log("Entering Scene is:" + next.buildIndex);
+        //if(next.buildIndex == 3) // main menu is 3
+        //{
+        //    if (activePlayer != null)
+        //    {
+        //        Destroy(activePlayer);
+        //        activePlayer = null;
+        //    }
+        //}
+        //else
+        //{
+
+        //}
+        playerSpawnSpot playerSpot = FindObjectOfType<playerSpawnSpot>();
+        if (activePlayer == null)
+        {
+            GameObject player = Instantiate(PlayerPrefab, playerSpot.transform.position, playerSpot.transform.rotation);
+            activePlayer = player.GetComponent<PlayerMovement>();
+        }
+        else
+        {
+            activePlayer.transform.position = playerSpot.transform.position;
+            activePlayer.transform.rotation = playerSpot.transform.rotation;
+        }
+    }
     void Update()
     {
         
