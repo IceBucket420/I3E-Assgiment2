d2cd84eec77a3b487f899ef49449b4547a55c339
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-06-23 16:40:48 +0800

    Update GameManager.cs
    Change player spawn on start instead of awake

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -13,7 +13,9 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject QuitMenu;
     public GameObject MainMenuCanvas;
+    public GameObject AudioSettingsCanvas;
     public int Level2;
+    public int MainMenu;
     public int audioSettings;
     public AudioSource bgm;
     public Slider MasterSlider;
@@ -24,21 +26,7 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI EffectsSound;
 
     // Start is called before the first frame update
-    private void Awake()
-    {
-        
-        if (instance != null && instance != this)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            DontDestroyOnLoad(gameObject);
-            SceneManager.activeSceneChanged += SpawnPlayerOnLoad;
-
-            instance = this;
-        }
-    }
+  
     private void SpawnPlayerOnLoad(Scene prev, Scene next)
     {
         Debug.Log("Entering Scene is:" + next.buildIndex);
@@ -61,6 +49,17 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(1);
         DontDestroyOnLoad(QuitMenu);
         MainMenuCanvas.gameObject.SetActive(false);
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            SceneManager.activeSceneChanged += SpawnPlayerOnLoad;
+
+            instance = this;
+        }
     }
 
     public void OnQuitButton()
@@ -82,6 +81,8 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(2);
         DontDestroyOnLoad(QuitMenu);
+        DontDestroyOnLoad(MainMenuCanvas);
+        MainMenuCanvas.gameObject.SetActive(false);
     }
 
     public void OnMasterSlider()
@@ -106,6 +107,12 @@ public class GameManager : MonoBehaviour
         {
             EffectsSound.text = v.ToString("0");
         });
+    }
+
+    public void OnBackButton()
+    {
+        SceneManager.LoadScene(3);
+        DontDestroyOnLoad(AudioSettingsCanvas);
     }
     private void Start()
     {
