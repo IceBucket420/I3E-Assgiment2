dbca3db7b192101d2407dde250752d9a4ab70cc5
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-06-24 16:51:30 +0800

    Update AudioManager.cs
    For the Audio to be affected in other scenes

--- scripts/AudioManager.cs
+++ scripts/AudioManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] AudioMixer mixer;
+    public static AudioManager instance;
+    public const string MASTER_KEY = "masterVolume";
+    public const string SFX_KEY = "sfxVolume";
+    public const string BGM_KEY = "bgmVolume";
+
+
+    private void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        LoadVolume();
     }
 
-    // Update is called once per frame
-    void Update()
+   
+    void LoadVolume()// volume saved in VolumeSettings.cs
     {
-        
+        float masterVolume = PlayerPrefs.GetFloat(MASTER_KEY, 1f);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, 1f);
+        float bgmVolume = PlayerPrefs.GetFloat(BGM_KEY, 1f);
+
+
+        mixer.SetFloat(VolumeSettings.MIXER_MASTER, Mathf.Log10(masterVolume) * 20);
+        mixer.SetFloat(VolumeSettings.MIXER_SFX, Mathf.Log10(sfxVolume) * 20);
+        mixer.SetFloat(VolumeSettings.MIXER_BGM, Mathf.Log10(bgmVolume) * 20);
     }
 }
