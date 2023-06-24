d798e54231c66dd0cd1efcee0651836202584336
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-06-24 16:51:50 +0800

    Update VolumeSettings.cs
    New changes to the script

--- scripts/VolumeSettings.cs
+++ scripts/VolumeSettings.cs
@@ -11,15 +11,29 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] Slider SFXSlider;
     [SerializeField] Slider BGMSlider;
 
-    const string MIXER_MASTER = "MasterVolume";
-    const string MIXER_SFX = "SFXVolume";
-    const string MIXER_BGM = "BGMVolume";
+    public const string MIXER_MASTER = "MasterVolume";
+    public const string MIXER_SFX = "SFXVolume";
+    public const string MIXER_BGM = "BGMVolume";
 
     private void Awake()
     {
         MasterSlider.onValueChanged.AddListener(SetMasterVolume);
         SFXSlider.onValueChanged.AddListener(SetSFXVolume);
         BGMSlider.onValueChanged.AddListener(SetBGMVolume);
+    }
+
+    private void Start()
+    {
+        MasterSlider.value = PlayerPrefs.GetFloat(AudioManager.MASTER_KEY, 1f);
+        SFXSlider.value = PlayerPrefs.GetFloat(AudioManager.SFX_KEY, 1f);
+        BGMSlider.value = PlayerPrefs.GetFloat(AudioManager.BGM_KEY, 1f);
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(AudioManager.MASTER_KEY, MasterSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_KEY, SFXSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.BGM_KEY, BGMSlider.value);
     }
 
     void SetMasterVolume(float value)
