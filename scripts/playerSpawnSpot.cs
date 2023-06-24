dea6cd22b74e83066bb412c132feec696a27e79a
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-06-24 18:38:47 +0800

    Update playerSpawnSpot.cs
    not much changes just additional spaces

--- scripts/playerSpawnSpot.cs
+++ scripts/playerSpawnSpot.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 
 public class playerSpawnSpot : MonoBehaviour
@@ -9,12 +6,12 @@ public class playerSpawnSpot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
