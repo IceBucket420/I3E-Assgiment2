0ad48e858f5ff33c77669e47eec469790b369c4e
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-06-28 12:07:40 +0800

    Update door.cs
    New code for animation for the door

--- scripts/door.cs
+++ scripts/door.cs
@@ -6,6 +6,8 @@ public class door : MonoBehaviour
 {
     public Animator animator;
     public Animator animator2;
+    public Animator animator3;
+    public Animator animator4;
     public AudioSource doorSound;
 
     private void OnTriggerEnter(Collider other)
@@ -13,6 +15,13 @@ public class door : MonoBehaviour
         animator.SetBool("isOpen", true);
         animator2.SetBool("isOpen", true);
         doorSound.Play();
+
+        if(other.gameObject.GetComponent<PlayerMovement>().Ready == true)
+        {
+            animator3.SetBool("isOpen", true);
+            animator4.SetBool("isOpen", true);
+            doorSound.Play();
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -20,5 +29,12 @@ public class door : MonoBehaviour
         animator.SetBool("isOpen", false);
         animator2.SetBool("isOpen", false);
         doorSound.Play();
+
+        if (other.gameObject.GetComponent<PlayerMovement>().Ready == true)
+        {
+            animator3.SetBool("isOpen", false);
+            animator4.SetBool("isOpen", false);
+            doorSound.Play();
+        }
     }
 }
